5b36ad18fb47ef9efeebbf709f70f47f76d090b1
omin0uschaos <10238707+omin0uschaos@users.noreply.github.com>
2024-02-25 00:11:30 -0800

    clean up code from new page

--- routes/register.js
+++ routes/register.js
@@ -3,37 +3,9 @@ const router = express.Router();
 const fs = require('fs');
 
 const users = require("../data/users");
+const ships = require("../data/ships");
+const makes = require("../data/makes");
 
-
-router.get("/:userId", (req, res) => {
-    const userId = req.params.userId;
-    fs.readFile('./data/users.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).send('Internal Server Error');
-        }
-        
-        const users = JSON.parse(data);
-        const user = users[userId];
-        if (!user) {
-            return res.status(404).send('User not found');
-        }
-
-        const options = {
-
-            title: `User Profile - ${user.username}`,
-            subTitle: `User ID: ${user.personData.name}`,
-            content: `
-                <h2>User Information</h2>
-                <p>Name: ${user.personData.name}</p>
-                <p>Email: ${user.personData.email}</p>
-                <p>Address: ${user.personData.Address}</p>
-                <!-- Add more user information as needed -->
-            `
-        };
-        res.render("index", options);
-    });
-});
 
 router.get("/register", (req, res) => {
     const userId = req.params.userId;
